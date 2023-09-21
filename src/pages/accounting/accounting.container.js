9ceee5a9916636b0e7660a6fbb5753b105a61aa8
happppi <102276917+hhbb0081@users.noreply.github.com>
2023-09-21 23:31:51 +0900

    Docs: 회계 화면 완성

---
+++ src/pages/accounting/accounting.container.js
@@ -0,0 +1,115 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import * as S from "./accounting.styles";
+import axios from "axios";
+import Menu from "../../components/nav/Nav";
+import AccountingComponent from "../../components/accounting/AccountingComponent";
+import ChargedComponent from "../../components/accounting/ChargedComponent";
+import ChargedDetailComponent from "../../components/accounting/ChargedDetailComponent";
+
+import status from "../../assets/status.png";
+import register from "../../assets/register.png";
+import moneyPlus from "../../assets/moneyPlus.png";
+import moneyMinus from "../../assets/moneyMinus.png";
+
+function Accounting () {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const baseUrl = "http://115.85.183.74:8090";
+    const [category, setCategory] = useState(1);
+    const [isDetail, setIsDetail] = useState(false);
+
+    useEffect(() => {
+        console.log(location.state)
+        if(location.state == 2){
+            setCategory(2);
+        }
+    }, [])
+    return(
+        <S.MainWrapper>
+            <Menu />
+            <S.TitleWrapper>
+                <S.TitleTxt>나의 모임</S.TitleTxt>
+                <S.GroupWrapper><S.GroupName>양파시 광산동</S.GroupName><S.GroupRole><S.RoleBox><img width="20px" height="20px" src={status}/><S.RoleTxt>참여 상태</S.RoleTxt></S.RoleBox>관리자</S.GroupRole></S.GroupWrapper>
+            </S.TitleWrapper>
+            <S.AttendanceWrapper>
+                <S.AttendanceNav>
+                    <S.AttendanceUl style={{'color': '#fff'}}>
+                        <S.AttendanceLi onClick={(e) => setCategory(1)}><S.AttendanceBox clicked={category === 1}>입출금 내역</S.AttendanceBox></S.AttendanceLi>
+                        <S.AttendanceLi onClick={(e) => setCategory(2)}><S.AttendanceBox clicked={category === 2}>청구 목록</S.AttendanceBox></S.AttendanceLi>
+                    </S.AttendanceUl>
+                </S.AttendanceNav>
+                {category === 1 ? (
+                    <>
+                    <S.ContentWrapper>
+                        <S.MoneyWrapper>
+                            <S.MoneyTitle>양파시 광산동의 머니</S.MoneyTitle>
+                            <S.MoneyTxt>200,000원</S.MoneyTxt>
+                        </S.MoneyWrapper>
+                        <S.MoneyHistoryWrapper>
+                            <S.MoneyHistoryTitle>입/출금 내역</S.MoneyHistoryTitle>
+                            <S.MoneyHistoryContent>
+                                <AccountingComponent/>
+                                <AccountingComponent/>
+                                <AccountingComponent/>
+                                <AccountingComponent/>
+                                <AccountingComponent/>
+                                <AccountingComponent/>
+                                <AccountingComponent/>
+                                <AccountingComponent/>
+                                <AccountingComponent/>
+                                <AccountingComponent/>
+                            </S.MoneyHistoryContent>
+                        </S.MoneyHistoryWrapper>
+                    </S.ContentWrapper>
+
+                    <S.AddBtnWrapper>
+                        <S.AddBtn onClick={() => navigate("/accounting/register")}>
+                            <img width='20px' height='20px' src={register} alt="register"/>
+                        </S.AddBtn>
+                    </S.AddBtnWrapper>
+                    </>
+                ) : (
+                    isDetail ? (
+                        <S.ContentWrapper>
+                            <S.ChargedDetailWrapper>
+                                <S.ChargedDetailTitle>신규 회비</S.ChargedDetailTitle>
+                                <S.ChargedDetailCategory>
+                                    <S.ChargedDetailName>이름</S.ChargedDetailName>
+                                    <S.ChargedDetailMoney>회비</S.ChargedDetailMoney>
+                                    <S.ChargedDetailChecked>납부여부</S.ChargedDetailChecked>
+                                </S.ChargedDetailCategory>
+                                <S.ChargedContentWrapper>
+                                    <ChargedDetailComponent/>
+                                    <ChargedDetailComponent/>
+                                    <ChargedDetailComponent/>
+                                </S.ChargedContentWrapper>
+                            </S.ChargedDetailWrapper>
+                        </S.ContentWrapper>
+                    ) : (
+                    <>
+                    <S.ContentWrapper>
+                        <ChargedComponent setIsDetail={setIsDetail}/>
+                        <ChargedComponent/>
+                        <ChargedComponent/>
+                        <ChargedComponent/>
+                        <ChargedComponent/>
+                        <ChargedComponent/>
+                        <ChargedComponent/>
+                    </S.ContentWrapper>
+                    <S.AddBtnWrapper>
+                        <S.AddBtn onClick={() => navigate("/charge/register")}>
+                            <img width='20px' height='20px' src={register} alt="register"/>
+                        </S.AddBtn>
+                    </S.AddBtnWrapper>
+                    </>
+                    )
+                )}
+            </S.AttendanceWrapper>
+            
+        </S.MainWrapper>
+    )
+}
+
+export default Accounting;
---
+++ src/pages/accounting/chargedRegister.container.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import * as S from "./chargedRegister.styles";
+import { useNavigate, useLocation } from "react-router-dom";
+import moment from "moment";
+import axios from "axios";
+
+import close from "../../assets/close.png";
+import clock from "../../assets/clock.png";
+import whiteMoneyCircle from "../../assets/whiteMoneyCircle.png";
+import moneyCircle from "../../assets/moneyCircle.png";
+
+function ChargedRegister (){
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const day = ['일', '월', '화', '수', '목', '금', '토'];
+    const [accountingTitle, setAccountingTitle] = useState("");
+    const [price, setPrice] = useState(0);
+    const date = new Date();
+    return(
+        <S.MainWrapper>
+            <S.CloseBtn><S.CloseImg src={close} onClick={(e) => navigate("/accounting", {state: 2})}/></S.CloseBtn>
+            <S.ContentWrapper>
+                <S.RegisterWrapper>
+                    <img src={moneyCircle} width="35px" height="35px"/>
+                    <S.RegisterTitle>청구 등록</S.RegisterTitle>
+                </S.RegisterWrapper>
+                <S.ScheduleTable>
+                    <S.ScheduleTr>
+                        <S.ScheduleFirstTd><S.Circle></S.Circle></S.ScheduleFirstTd>
+                        <S.ScheduleTd>
+                            <S.TitleInput 
+                                placeholder="내역 이름" 
+                                value={accountingTitle} 
+                                onChange={(e) => setAccountingTitle(e.target.value)}
+                            />
+                        </S.ScheduleTd>
+                    </S.ScheduleTr>
+                    <S.ScheduleTr>
+                        <S.ScheduleFirstTd><img src={clock}/></S.ScheduleFirstTd>
+                        <S.ScheduleTd>
+                            <S.DateTxt>{location.state}</S.DateTxt>
+                            <S.TimeTxt>
+                                {moment(date).format("YYYY년 M월 D일 ")}{day[moment(date).format("e")]}요일
+                            </S.TimeTxt>
+                        </S.ScheduleTd>
+                    </S.ScheduleTr>
+                    <S.ScheduleTr>
+                        <td><img width="18px" height="18px" src={whiteMoneyCircle}/></td>
+                        <S.ScheduleTd>
+                            <S.ContentInput 
+                                placeholder="금액" 
+                                value={price}
+                                onChange={(e) => {
+                                    const inputPrice = e.target.value.replace(/\D/g, '');
+                                    const formattedPrice = inputPrice.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+                                    setPrice(formattedPrice);
+                                }}
+                            />
+                            <S.ContentWon>원</S.ContentWon>
+                        </S.ScheduleTd>
+                    </S.ScheduleTr>
+                </S.ScheduleTable>
+            </S.ContentWrapper>
+            <S.BtnWrapper>
+                <S.ModifyBtn>등록하기</S.ModifyBtn>
+            </S.BtnWrapper>
+        </S.MainWrapper>
+    );
+}
+
+export default ChargedRegister;
